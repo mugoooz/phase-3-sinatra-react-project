189fbcd13613c32bc396d01c5af7c4483187a55a
Eric Mugo <mugoericpg20@gmail.com>
2023-06-08 23:42:29 +0300

    migrated weather

--- db/migrate/20230608204030_create_weathers.rb
+++ db/migrate/20230608204030_create_weathers.rb
@@ -1,5 +1,15 @@
 class CreateWeathers < ActiveRecord::Migration[6.1]
   def change
-    
+    create_table :weathers do |t|
+      t.string :city
+      t.string :weather
+      t.string :description
+      t.string :icon
+      t.float :temperature
+      t.float :min_temp
+      t.float :max_temp
+
+      t.timestamps
+    end
   end
 end
