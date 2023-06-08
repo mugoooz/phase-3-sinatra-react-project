5c0066dd64f8ca9801a6d9552011a06130d66a8b
Eric Mugo <mugoericpg20@gmail.com>
2023-06-08 23:48:33 +0300

    seeded nairobi

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,15 @@ City.create(name: 'Nairobi')
 City.create(name: 'New York')
 City.create(name: 'London')
 
+Weather.create(
+  city_id: 1,
+  main: 'Clouds',
+  description: 'Few clouds',
+  icon: '02d',
+  temperature: 25.5,
+  temp_min: 22.1,
+  temp_max: 28.3
+)
+
+
 puts "✅ Done seeding!"
